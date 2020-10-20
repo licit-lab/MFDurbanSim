63258e930d7c6555fcbd9e0971b1492ca6ffbc5a
a-male <amale@groupeastek.fr>
2020-10-20 10:53:11 +0200

    No large files Demand completed

--- examples/Lyon10Res/Script_demand.py
+++ examples/Lyon10Res/Script_demand.py
@@ -1,53 +1,36 @@
-import math
 import json
-import collections
 
 with open("RoutesStruct_Matlab.json", "r") as file:
     routes_matlab = json.load(file)
 
-with open("Network.json", "r") as file:
-    network = json.load(file)
+with open("ODmacroStruct_Matlab.json", "r") as file:
+    od_matlab = json.load(file)
 
-demand_tmp = []
-routes_per_node_couple = []
+Demand = {"FLOW DEMAND": []}
 
-for route in range(len(routes_matlab)):
-    node_origin = routes_matlab[route]["NodePath"][0]
-    node_dest = routes_matlab[route]["NodePath"][-1]
-
-    routes_per_node_couple.append({"node_pair": [node_origin, node_dest], "routes": []})
-
-    for route2 in range(len(routes_matlab)):
-        node_origin2 = routes_matlab[route2]["NodePath"][0]
-        node_dest2 = routes_matlab[route2]["NodePath"][-1]
-
-        if node_origin2 == node_origin and node_dest2 == node_dest:
-            routes_per_node_couple[route]["routes"].append({"id": route2+1, "assign_coeff": routes_matlab[route2]["AssignCoeff"]})
-
-routes_per_node_couple_new = []
-for item in routes_per_node_couple:
-    if item not in routes_per_node_couple_new:
-        routes_per_node_couple_new.append(item)
-
-#Open file
-with open("Demand.json", "r") as file:
-    Demand = json.load(file)
-
-Demand["FLOW DEMAND"] = []
-
-for item in routes_per_node_couple_new:
+for od in od_matlab:
     data_str = []
     route_str = []
-    for route in item["routes"]:
-        data_str.append({"ID": "Route" + str(route["id"]), "Coeff": route["assign_coeff"]})
+
+    # Recover assignment coefficients corresponding to a route id
+    if isinstance(od["RoutesID"], list):
+        for route_id in od["RoutesID"]:
+            data_str.append({"ID": "Route" + str(route_id), "Coeff": routes_matlab[route_id - 1]["AssignCoeff"]})
+    elif isinstance(od["RoutesID"], int):
+        data_str.append({"ID": "Route" + str(od["RoutesID"]), "Coeff": routes_matlab[od["RoutesID"] - 1]["AssignCoeff"]})
 
     route_str.append({"Time": 0, "Data": data_str})
 
-    Demand["FLOW DEMAND"].append({"OriginMacroNodeID": "MacroNode" + str(item["node_pair"][0]),
-                                  "DestMacroNodeID": "MacroNode" + str(item["node_pair"][-1]),
-                                  "Demand": [],
+    demand_tmp = [{"Time": od["Demand"]["Time"][0], "Data": od["Demand"]["Data"][0]}]
+
+    for j in range(1, len(od["Demand"]["Time"])):
+        if od["Demand"]["Data"][j] != od["Demand"]["Data"][j-1] and od["Demand"]["Time"][j] < 1802:
+            demand_tmp.append({"Time": od["Demand"]["Time"][j], "Data": od["Demand"]["Data"][j]})
+
+    Demand["FLOW DEMAND"].append({"OriginMacroNodeID": "MacroNode" + str(od["NodeOriginID"]),
+                                  "DestMacroNodeID": "MacroNode" + str(od["NodeDestinationID"]),
+                                  "Demand": demand_tmp,
                                   "Route": route_str})
 
-
 with open("Demand.json", "w") as file:
-    json.dump(Demand, file, indent = 4)
+    json.dump(Demand, file, indent=4)
