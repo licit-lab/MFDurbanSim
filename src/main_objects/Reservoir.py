50fd802906fc0a5a3ee4766b56afa5e0cb994a12
a-male <amale@groupeastek.fr>
2020-12-01 11:53:05 +0100

    Reservoir.py: Delete useless comments. Correction in get_MFD_setting function to get settings correctly in a multi-modality scenario. Some variables renamed to fit python convention. Script_Visualisation.py: Added in Braess_2modes/DemSC1.

--- src/main_objects/Reservoir.py
+++ src/main_objects/Reservoir.py
@@ -1,52 +1,48 @@
-import copy
 from main_objects.Element import Element
 
 
 def get_reservoir(reservoirs, reservoir_id):
-    
     for r in reservoirs:
         if r.ID == reservoir_id:
             return r
         
     return None
 
+
 class Reservoir(Element):
-    
     def __init__(self):
+        data_keys = ["Time",
+                            
+                     # Accumulation based model
+                     "InternalProd",
+                     "MeanSpeed",
+                     "AvgTripLength",      # average trip length (m) of routes originating outside reservoir only
+                     "Acc",                # accumulation (veh)
+                     "ProductionSupply",   # modified entry production supply (veh.m/s)
+                     "Inflow",
+                     "Outflow",
+                     "Nin",
+                     "Nout",
+                     "MeanSpeed",
+                     "Demand"
+                            
+                     # Trip based model
+                     # "PossibleNextEntryTime"
+                     # "PossibleNextExitTime"
+                     # "TotalTraveledDistance"
+
+                     ]
         
-        DataKeys = [
-                            "Time",           
-                            
-                            # Accumulation based model
-                            "InternalProd", 
-                            "MeanSpeed", 
-                            "AvgTripLength",      # average trip length (m) corresponding to the routes orignating outside reservoir only
-                            "Acc",                # accumulation (veh)
-                            "ProductionSupply",   # modified entry production supply (veh.m/s)
-                            "Inflow",
-                            "Outflow", 
-                            "Nin", 
-                            "Nout",
-                            "MeanSpeed",
-                            "Demand"
-                            
-                            # Trip based model
-                            #"PossibleNextEntryTime"
-                            #"PossibleNextExitTime"
-                            #"TotalTraveledDistance"
-                            
-                            
-                            ]
-        
-        Element.__init__(self, DataKeys)
+        Element.__init__(self, data_keys)
         
         # Input
-        self.ID = ""                        #ID of the reservoir
+        self.ID = ""                        # ID of the reservoir
         
-        self.MFDsetting = []                # Setting of the MFD by mode (mode, free flow speed, max production, max accumulation)
-      
-        self.Centroid = None                # Abscissa and ordinate of reservoir center (plotting purpose)
-        self.BorderPoints = None            # List of abscissa and ordinates of the N points delimiting the reservoir (plotting purpose)
+        self.MFDsetting = []                # MFD setting by mode (mode, free flow speed, max prod, max acc)
+
+        # Plotting purpose
+        self.Centroid = None                # Coordinates of reservoir center
+        self.BorderPoints = None            # List of coordinates of the points delimiting the reservoir
 
         # Both solvers
         self.MFDfctParam = []               # Parameters of the MFD function
@@ -61,7 +57,8 @@ class Reservoir(Element):
         
         # Acc-based solver
         # to keep ?
-        self.NumWaitingVeh = []             # Number of vehicles waiting to enter the reservoir when reservoir is the begining of the route
+        self.NumWaitingVeh = []             # Number of vehicles waiting to enter the reservoir when reservoir is
+                                            # the beginning of the route
 
         # Trip-based solver
         self.t_in = -1
@@ -73,87 +70,65 @@ class Reservoir(Element):
         self.VehList = []                   # List of vehicles in the reservoir
 
     def load_input(self, load_network, i):
-        self.ID = load_network["RESERVOIRS"][i]["ID"]
+        load_res = load_network["RESERVOIRS"][i]
+        self.ID = load_res["ID"]
+
+        for m in load_res["FreeflowSpeed"]:
+            mfd_set = {'mode': m['mode'], 'FreeflowSpeed': m['value'],
+                       'MaxProd': [tag for tag in load_res["MaxProd"] if tag['mode'] == 'VL'][0]['value'],
+                       'MaxAcc': [tag for tag in load_res["MaxAcc"] if tag['mode'] == 'VL'][0]['value'],
+                       'CritAcc': [tag for tag in load_res["CritAcc"] if tag['mode'] == m['mode']][0]['value']}
+            self.MFDsetting.append(mfd_set)
         
-        MFDs = {}
-        
-        for m in load_network["RESERVOIRS"][i]["FreeflowSpeed"]:
-            MFDs['mode'] = m['mode']
-            MFDs['FreeflowSpeed'] = m['value']
-            MFDs['MaxProd'] = [tag for tag in load_network["RESERVOIRS"][i]["MaxProd"] if tag['mode']=='VL'][0]['value']
-            MFDs['MaxAcc'] = [tag for tag in load_network["RESERVOIRS"][i]["MaxAcc"] if tag['mode']=='VL'][0]['value']
-            MFDs['CritAcc'] = [tag for tag in load_network["RESERVOIRS"][i]["CritAcc"] if tag['mode']=='VL'][0]['value']
-           
-        self.MFDsetting.append(MFDs)
-        
-        if 'Centroid' in load_network["RESERVOIRS"][i]:
-            self.Centroid = load_network["RESERVOIRS"][i]["Centroid"]
+        if 'Centroid' in load_res:
+            self.Centroid = load_res["Centroid"]
             
-        if 'BorderPoints' in load_network["RESERVOIRS"][i]:
-            self.BorderPoints = load_network["RESERVOIRS"][i]["BorderPoints"]
+        if 'BorderPoints' in load_res:
+            self.BorderPoints = load_res["BorderPoints"]
 
 
     def get_production_from_accumulation(self, n, mode):
-        
-        # nj = param(1); % jam accumulation (max. accumulation) [veh]
-        # nc = param(2); % critical accumulation [veh]
-        # Pc = param(3); % critical production (max. production) [veh.m/s]
-        
-        # P = (0 <= n).*(n <= nc).* (Pc./nc.^2.*n.*(2*nc - n)) + ...
-        #     (nc < n).*(n < nj).*  (Pc./(nj - nc).^2.*(nj - n).*(nj + n - 2*nc));
-                
-        MFDset = [tag for tag in self.MFDsetting if tag['mode'] == mode][0]
-        
-        production = (0<=n)*(n<=MFDset['CritAcc'])*(MFDset['MaxProd']/(MFDset['CritAcc']**2)*n*(2*MFDset['CritAcc']-n)) + \
-                    (MFDset['CritAcc']<n)*(n<MFDset['MaxAcc'])*(MFDset['MaxProd']/(MFDset['MaxAcc']-MFDset['CritAcc'])**2*(MFDset['MaxAcc']-n)*(MFDset['MaxAcc']+n-2*MFDset['CritAcc']))
-        
-        # if accumulation>= MFDset['MaxAcc'] or accumulation<=0.:
-        #     return 0.
-        
-        # a = MFDset['MaxProd'] / (MFDset['CritAcc']*(MFDset['CritAcc']-MFDset['MaxAcc']))
-        # b = -a * MFDset['MaxAcc']
-        # c = 0
-        # return ( (a*accumulation**2) + (b*accumulation) + c)
-    
+        mfd_set = [tag for tag in self.MFDsetting if tag['mode'] == mode][0]
+        crit_acc = mfd_set['CritAcc']
+        max_prod = mfd_set['MaxProd']
+        max_acc = mfd_set['MaxAcc']
+
+        production = (0 <= n) * (n <= crit_acc) * (max_prod / (crit_acc ** 2) * n * (2 * crit_acc - n)) + \
+                     (crit_acc < n) * (n < max_acc) * (max_prod / (max_acc - crit_acc) ** 2 * (max_acc - n) *
+                                                       (max_acc + n - 2 * crit_acc))
+
         return production
-    
+
+
     def get_speed_from_accumulation(self, accumulation, mode):
-        MFDset = [tag for tag in self.MFDsetting if tag['mode']==mode][0]
+        mfd_set = [tag for tag in self.MFDsetting if tag['mode'] == mode][0]
         
-        if accumulation>0:
-            production = self.get_production_from_accumulation(accumulation,mode)
+        if accumulation > 0:
+            production = self.get_production_from_accumulation(accumulation, mode)
             return production / accumulation
         
-        return MFDset['FreeflowSpeed']
+        return mfd_set['FreeflowSpeed']
 
 
     def get_MFD_setting(self, data, mode):
-        MFDset = [tag for tag in self.MFDsetting if tag['mode']==mode][0]
-        return MFDset[data]
-        
+        mfd_set = [tag for tag in self.MFDsetting if tag['mode'] == mode][0]
+        return mfd_set[data]
+
+
     def get_entry_supply_from_accumulation(self, accumulation, mode):
-        
-        MFDset = [tag for tag in self.MFDsetting if tag['mode'] == mode][0]
-        # a1 = 0.8
-        # a2 = 1
-        # b = 1
-        
-        a1=0.5
-        a2=1.5
+        mfd_set = [tag for tag in self.MFDsetting if tag['mode'] == mode][0]
+
+        a1 = 0.5
+        a2 = 1.5
         b = 1.4
         
-        param=[ MFDset['MaxAcc'], MFDset['CritAcc'], MFDset['MaxProd'], a1*MFDset['CritAcc'], a2*MFDset['CritAcc'], b*MFDset['MaxProd'] ]
+        param = [mfd_set['MaxAcc'], mfd_set['CritAcc'], mfd_set['MaxProd'],
+                 a1 * mfd_set['CritAcc'], a2 * mfd_set['CritAcc'], b * mfd_set['MaxProd']]
         
-        entry_supply =  (accumulation <= param[3]) * param[5] + \
-                        (param[3]< accumulation)*(accumulation<=param[4]) * \
-                        (param[5]+(accumulation-param[3])/(param[4]-param[3])*(self.get_production_from_accumulation(a2*MFDset['CritAcc'], mode)-param[5])) + \
-                        (param[4]< accumulation)*self.get_production_from_accumulation(accumulation, mode)
-            
-        
-        # % Entry supply function
-        # % param = [nj nc Pc a1*nc a2*nc b*Pc], with 0 < a1 < 1 < a2, and 1 < b
-        # Entryfct = @(n,param) (n <= param(4)).*param(6) + ...
-        # (param(4) < n).*(n <= param(5)).*(param(6)+(n-param(4))./(param(5)-param(4)).*(MFDfct(param(5),param(1:3))-param(6))) + ...
-        # (param(5) < n).*MFDfct(n,param(1:3));
-        
+        entry_supply = (accumulation <= param[3]) * param[5] + \
+                       (param[3] < accumulation) * (accumulation <= param[4]) * \
+                       (param[5] + (accumulation-param[3]) / (param[4] - param[3]) *
+                        (self.get_production_from_accumulation(a2 * mfd_set['CritAcc'], mode)-param[5])) + \
+                       (param[4] < accumulation) * self.get_production_from_accumulation(accumulation, mode)
+
         return entry_supply
